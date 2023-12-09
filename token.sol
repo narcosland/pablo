90fa6ece85f9b4035cfaa318240d7e4dbb874db7
narcosland <153362521+narcosland@users.noreply.github.com>
2023-12-09 18:18:28 +0500

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,7 +1,3 @@
-/**
- *Submitted for verification at Etherscan.io on 2023-11-13
-*/
-
 // SPDX-License-Identifier: MIT
 // OpenZeppelin Contracts (last updated v5.0.0) (token/ERC20/ERC20.sol)
 //Developed by: NarcosLand
